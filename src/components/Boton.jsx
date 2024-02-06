209385e69f6b3935a5a695efc9d921417c2b142a
Bruno Cordoba <brunonahuel87@gmail.com>
2024-02-06 19:15:33 -0300

    agregue el estado para cambiar el titulo

--- src/components/Boton.jsx
+++ src/components/Boton.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 
 const Boton = () => {
+    const [Texto,setTexto] = useState('');
     return (
-        <div>
-            <h1>Hello my friend</h1>
-           
+        <div className="text-center">
+            <h1>Hello my friend {Texto} </h1>
+           <button className="btn btn-primary my-4" onClick={() =>{
+             setTexto('(from changed state)');
+           }}>Click my</button>
         </div>
     );
 };
